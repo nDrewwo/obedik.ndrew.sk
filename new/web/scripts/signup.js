4c750af5fac7b72b5ac13f6d62296087e2b5fdc5
ndrew <andrej.siekel@gmail.com>
2024-06-21 17:31:26 +0200

    cleanup + added burza page

---
+++ new/web/scripts/signup.js
@@ -0,0 +1,17 @@
+document.getElementById('registerForm').addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+    const email = document.getElementById('email').value;
+
+    const response = await fetch('http://localhost:3000/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ username, password, email })
+    });
+
+    const data = await response.json();
+    alert(data.message);
+});
